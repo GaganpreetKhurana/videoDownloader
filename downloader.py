f226be2d2f17c4a16d953a716d7eb2bf82afdc12
dark-knight1729 <giftykhurana0@gmail.com>
2020-05-01 14:54:18 +0530

    script complete

--- downloader.py
+++ downloader.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 import youtube_dl
 
+count = 0
+
 
 def extractType(infoDict):
     return infoDict.get('_type', None)
@@ -9,16 +11,21 @@ def extractType(infoDict):
 
 def extractDetailsSingle(infoDict, url):
     details = dict()
+    global count
     details['title'] = infoDict['title']
     details['url'] = url
     for eachFormat in infoDict['formats']:
         if eachFormat['acodec'] != 'none' and (eachFormat['ext'] == 'mp4' or eachFormat['ext'] == 'm4a'):
             detailsEachFormat = dict()
             detailsEachFormat['formatId'] = eachFormat['format_id']
+            detailsEachFormat['format'] = eachFormat['format_note']
             detailsEachFormat['extension'] = eachFormat['ext']
+            detailsEachFormat['url'] = url
             detailsEachFormat['videoAvailable'] = eachFormat['vcodec'] != 'none'
             detailsEachFormat['audioAvailable'] = eachFormat['acodec'] != 'none'
-            details[eachFormat['format_note']] = detailsEachFormat
+            details[count] = detailsEachFormat
+
+            count += 1
     return details
 
 
@@ -46,14 +53,61 @@ def printPlaylistDetails(details):
         print()
 
 
-def extractor(link='https://www.youtube.com/watch?v=oUUyuW5PnhM&list=PL4x7Of-X4XhihdpCA1wCUNB6KEp1FNVms'):
+def printSingleMenu(details):
+    print("Title: ", details['title'])
+    for eachFormat in details:
+        if eachFormat != 'title' and eachFormat != 'url':
+            print(eachFormat, details[eachFormat]['format'])
+
+
+def printPlaylistMenu(details):
+    print('Playlist Title: ', details['playlistTitle'])
+    print()
+    for eachSingle in details['singles']:
+        printSingleMenu(details['singles'][eachSingle])
+        print()
+
+
+def download(details, options):
+    options['skip_download'] = True
+    print()
+    try:
+        singlesToDownload = list(map(int, input("List ID's of Videos to download separated by space: ").split()))
+    except:
+        print("Invalid")
+        return
+    if 'singles' in details:
+        urlList = dict()
+        details = details['singles']
+        for titles in details:
+            for id in details[titles]:
+                if id == 'title' or id == 'url':
+                    continue
+                urlList[id] = {'url': details[titles][id]['url'], 'title': titles,
+                               'formatId': details[titles][id]['formatId']}
+    else:
+        urlList = details
+    for id in singlesToDownload:
+        print("Trying", id)
+        if id in urlList:
+            if 'title' in urlList[id]:
+                print('Downloading : ', urlList[id]['title'], " :: ", id)
+            else:
+                print('Downloading : ', id)
+            options['format'] = urlList[id]['formatId']
+            with youtube_dl.YoutubeDL(options) as ydl:
+                ydl.download([urlList[id]['url']])
+            print('Downloaded : ', id)
+
+
+def extractor(link):
     options = {
         'quiet': True,
         'no_warnings': True,
         'ignoreerrors': True,
-        'writesubtitles': True,
-        'writeautomaticsub': True,
-        'allsubtitles': True,
+        'writesubtitles': False,
+        'writeautomaticsub': False,
+        'allsubtitles': False,
         'skip_download': True,
     }
     with youtube_dl.YoutubeDL(options) as ydl:
@@ -61,11 +115,23 @@ def extractor(link='https://www.youtube.com/watch?v=oUUyuW5PnhM&list=PL4x7Of-X4X
     type = extractType(infoDict)
     if type is None:
         details = extractDetailsSingle(infoDict, link)
-        printSingleDetails(details)
+        # printSingleDetails(details)
+        printSingleMenu(details)
     else:
         details = extractDetailsPlaylist(infoDict, link)
-        printPlaylistDetails(details)
+        # printPlaylistDetails(details)
+        printPlaylistMenu(details)
+    download(details.copy(), options.copy())
 
 
-# extractor('https://www.youtube.com/watch?v=oUUyuW5PnhM')
-extractor()
+print('Press "-" to QUIT')
+url = input('Enter Playlist/Video Url: ')
+# url='https://www.youtube.com/watch?v=oUUyuW5PnhM&list=PL4x7Of-X4XhihdpCA1wCUNB6KEp1FNVms'
+# url='https://www.youtube.com/watch?v=oUUyuW5PnhM'
+if url == '-':
+    print("Good Bye")
+else:
+    try:
+        extractor(url)
+    except:
+        print("An Error Occurred")
